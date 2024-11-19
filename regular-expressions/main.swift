7deae8c1e7b59ac5c1bb21404115e39a7cec829f
ioanaydgn <mertaydogn0@gmail.com>
2024-11-19 13:14:40 +0200

    Add function to find login requests in log data

--- regular-expressions/main.swift
+++ regular-expressions/main.swift
@@ -23,6 +23,21 @@ func readLogFile(fileName: String) -> [String]? {
     }
 }
 
+//MARK: --FIND LOGIN REQUEST
+func findLoginRequests(logs: [String]) -> [String] {
+    let pattern = "(https://www\\.google\\.com|https://www\\.bing\\.com)"
+    let regex = try! NSRegularExpression(pattern: pattern)
+    var results = [String]()
+    
+    for log in logs {
+        let range = NSRange(log.startIndex..., in: log)
+        if regex.firstMatch(in: log, options: [], range: range) != nil {
+            results.append(log)
+        }
+    }
+    return results
+}
+
 
 
 // MARK: --TEST FUNCTIONS
@@ -31,7 +46,18 @@ func generateReport(fileName: String) {
     print("Logs taken from file: \(fileName)")
     
     if let logs = readLogFile(fileName: fileName) {
+        
+        //Logs
         print("Logs successfully read. Total logs: \(logs.count)")
+        
+        //Request Login Page
+        print("\nRequests to the login page:")
+                let loginRequests = findLoginRequests(logs: logs)
+                if loginRequests.isEmpty {
+                    print("No login requests found.")
+                } else {
+                    loginRequests.forEach { print($0) }
+                }
     }
 }
 let logFile = "/Users/aydgnme/Courses/PrT/labs/lab4/access_lab4.log" // Replace with your actual log file path
