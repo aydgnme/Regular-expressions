7ebd27430ea8c99d3f9936504a84bbe46f0f502b
ioanaydgn <mertaydogn0@gmail.com>
2024-11-19 13:10:43 +0200

    Add function to read log file and handle errors

--- regular-expressions/main.swift
+++ regular-expressions/main.swift
@@ -7,5 +7,19 @@
 
 import Foundation
 
-print("Hello, World!")
+//MARK: --READ LOG FILE
+func readLogFile(fileName: String) -> [String]? {
+    let fileManager = FileManager.default
+    // Get the file URL from the current directory
+    let fileURL = URL(fileURLWithPath: fileName)
+    
+    // Try to read the contents of the file
+    do {
+        let fileContent = try String(contentsOf: fileURL, encoding: .utf8)
+        return fileContent.components(separatedBy: "\n")
+    } catch {
+        print("Failed to read file at \(fileName): \(error)")
+        return nil
+    }
+}
 
